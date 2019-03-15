8eb25449adadbd23878eda9d71e3577ea3ebc015
lkoppers <lars.koppers@tu-dortmund.de>
2019-03-15 09:22:55 +0100

    seed correction in test_topicCoherence

--- tests/testthat/test_topicCoherence.R
+++ tests/testthat/test_topicCoherence.R
@@ -2,16 +2,17 @@ context("topicCoherence")
 
 test_that("topicCoherence", {
 
+suppressWarnings(RNGversion("3.5.0"))
+set.seed(24601)
+  
 docs <- lapply(1:100, function(x)rbind(sample(0L:99L, 50, replace=TRUE), 1L))
 docs <- c(docs, lapply(1:100, function(x)rbind(sample(50L:149L, 50, replace=TRUE), 1L)))
 
-suppressWarnings(RNGversion("3.5.0"))
-set.seed(24601)
 ldaresult <- LDAgen(documents=docs, K = 5L, vocab=as.character(0:149), num.iterations = 20L, burnin = 70L, seed=24602, num.words = 10L, LDA = TRUE, count=TRUE)
 
-expect_equal(round(topicCoherence(ldaresult=ldaresult, documents=docs, num.words=10, by.score=TRUE),5), c(-41.23167, -40.07306, -37.49477, -47.19478, -34.73816))
-expect_equal(round(topicCoherence(ldaresult=ldaresult, documents=docs, num.words=5, by.score=TRUE),5), c(-8.82447,  -9.10893,  -8.18058, -11.22174,  -8.41985))
-expect_equal(round(topicCoherence(ldaresult=ldaresult, documents=docs, num.words=5, by.score=FALSE),5), c(-8.82644,  -9.08993, -12.66089, -10.72207,  -7.63619))
+expect_equal(round(topicCoherence(ldaresult=ldaresult, documents=docs, num.words=10, by.score=TRUE),4), round(c(-41.74625, -38.26620, -34.25354, -35.56694, -47.67926),4))
+expect_equal(round(topicCoherence(ldaresult=ldaresult, documents=docs, num.words=5, by.score=TRUE),4), round(c(-9.279865,  -7.622130,  -7.338766,  -6.881622, -11.796214),4))
+expect_equal(round(topicCoherence(ldaresult=ldaresult, documents=docs, num.words=5, by.score=FALSE),4), round(c(-10.550004,  -7.724558,  -9.466624, -10.080576, -11.122454),4))
 expect_error(topicCoherence(ldaresult=ldaresult, documents=docs[[-1]], num.words=5, by.score=FALSE))
 expect_error(topicCoherence(ldaresult=ldaresult, documents=docs, num.words="5", by.score=FALSE))
 
