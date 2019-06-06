3f5623588c9fad6f4b57fbea323d2c68cf76c411
Jonas Rieger <jonas.rieger@tu-dortmund.de>
2019-06-06 11:04:45 +0200

    rewrite filter methods as S3 methods

--- R/filterCount.R
+++ R/filterCount.R
@@ -1,6 +1,6 @@
 #' Subcorpus With Count Filter
 #'
-#' Generates a subcorpus by restricting it to texts containing a specific number of words
+#' Generates a subcorpus by restricting it to texts containing a specific number of words.
 #'
 #' @param object A \code{\link{textmeta}} object
 #' @param text Not necassary if \code{object} is specified, else should be
@@ -9,9 +9,11 @@
 #' @param out Type of output: \code{text} filtered corpus,
 #' \code{bin} logical vector for all texts,
 #' \code{count} the counts.
+#' @param filtermeta Logical: Should the meta component be filtered, too?
 #' @return \code{\link{textmeta}} object if \code{object} is specified,
 #' else only the filtered \code{text}. If a \code{\link{textmeta}} object is
-#' returned its meta data are filtered to those texts which appear in the corpus.
+#' returned its meta data are filtered to those texts which appear in the corpus
+#' by default (\code{filtermeta}).
 #' @examples
 #' texts <- list(A="Give a Man a Fish, and You Feed Him for a Day.
 #' Teach a Man To Fish, and You Feed Him for a Lifetime",
@@ -20,36 +22,50 @@
 #' in evaluating complicated multiple integrals.")
 #'
 #' filterCount(text=texts, count=10L)
-#'
 #' filterCount(text=texts, count=10L, out="bin")
-#'
 #' filterCount(text=texts, count=10L, out="count")
 #' @export filterCount
-filterCount <- function(object, text, count = 1L, out = c("text", "bin", "count")){
+filterCount <- function(...) UseMethod("filterCount")
 
+#' @rdname filterCount
+#' @export
+filterCount.default <- function(text, count = 1L, out = c("text", "bin", "count")){
+  
   returnTextmeta <- FALSE
   if (!missing(object)){
     stopifnot(is.textmeta(object))
     text <- object$text
     returnTextmeta <- TRUE
   }
-
+  
   stopifnot(is.textmeta(textmeta(text = text)), as.integer(count) == count,
     all(out %in% c("text", "bin", "count")))
-
+  
   counts <- stringr::str_count(unlist(lapply(lapply(text, unlist),
     function(x) paste(x, collapse = " "))), pattern = "\\b[a-z,A-Z](.*?)\\b")
-
+  
   subid <- counts >= count
   subid[is.na(subid)] <- FALSE
-  if(out[1] == "text"){
-    if(returnTextmeta){
-      object$text <- text[subid]
-      object$meta <- object$meta[object$meta$id %in% names(object$text), ]
-      return(object)
-    }
-    return(text[subid])
-  }
+  if(out[1] == "text") invisible(text[subid])
   if(out[1] == "bin") return(subid)
   if(out[1] == "count") return(counts)
+}
+
+#' @rdname filterCount
+#' @export
+filterCount.textmeta <- function(object, count = 1L, out = c("text", "bin", "count"), filtermeta = TRUE){
+  
+  stopifnot(is.textmeta(object), is.logical(filtermeta), length(filtermeta) == 1)
+  
+  res = filterCount.default(text = object$text, count = count, out = out)
+  
+  if(out[1] == "text"){
+    object$text = res
+    if(filtermeta){
+      object$meta <- object$meta[object$meta$id %in% names(object$text),]
+    }
+    return(object)
+  }else{
+    return(res)
+  }
 }
--- R/filterDate.R
+++ R/filterDate.R
@@ -1,6 +1,6 @@
 #' Subcorpus With Date Filter
 #'
-#' Generates a subcorpus by restricting it to a specific time window
+#' Generates a subcorpus by restricting it to a specific time window.
 #'
 #' @param object \code{\link{textmeta}} object
 #' @param text Not necessary if \code{object} is specified, else should be
@@ -9,9 +9,11 @@
 #' \code{object$meta}
 #' @param s.date Start date of subcorpus as date object
 #' @param e.date End date of subcorpus as date object
+#' @param filtermeta Logical: Should the meta component be filtered, too?
 #' @return \code{\link{textmeta}} object if \code{object} is specified,
 #' else only the filtered \code{text}. If a \code{\link{textmeta}} object is
-#' returned its meta data are filtered to those texts which appear in the corpus.
+#' returned its meta data are filtered to those texts which appear in the corpus
+#' by default (\code{filtermeta}).
 #' @keywords manip
 #' @examples
 #' texts <- list(A="Give a Man a Fish, and You Feed Him for a Day.
@@ -29,21 +31,37 @@
 #' subcorpus$meta
 #' subcorpus$text
 #' @export filterDate
-filterDate <- function(object, text = object$text, meta = object$meta,
+filterDate <- function(...) UseMethod("filterDate")
+
+#' @rdname filterDate
+#' @export
+filterDate.default <- function(text, meta,
   s.date = min(meta$date, na.rm = TRUE),
   e.date = max(meta$date, na.rm = TRUE)){
+  
+  object <- textmeta(meta = meta, text = text)
+  object <- filterDate.textmeta(object = object, s.date = s.date, e.date = e.date, filtermeta = FALSE)
+  invisible(object$text)
+}
 
-  returnTextmeta <- TRUE
-  if(missing(object)){
-    object <- textmeta(meta = meta, text = text)
-    returnTextmeta <- FALSE
-  }
-  stopifnot(is.textmeta(object), length(s.date) == 1, length(e.date) == 1)
-  object$meta <- object$meta[match(names(object$text), object$meta$id),]
-  ind <- object$meta$date >= s.date & object$meta$date <= e.date
+#' @rdname filterDate
+#' @export
+filterDate.textmeta <- function(object,
+  s.date = min(meta$date, na.rm = TRUE),
+  e.date = max(meta$date, na.rm = TRUE),
+  filtermeta = TRUE){
+
+  stopifnot(is.textmeta(object), length(s.date) == 1, length(e.date) == 1,
+    is.logical(filtermeta), length(filtermeta) == 1)
+  
+  meta <- object$meta[match(names(object$text), object$meta$id),]
+  ind <- meta$date >= s.date & meta$date <= e.date
   ind[is.na(ind)] <- FALSE
-  object$meta <- object$meta[ind, ]
-  object$text <- object$text[match(object$meta$id, names(object$text))]
-  if(returnTextmeta) return(object)
-  return(object$text)
+  meta <- meta[ind, ]
+  
+  object$text <- object$text[match(meta$id, names(object$text))]
+  if(filtermeta){
+    object$meta <- meta
+  }
+  return(object)
 }
