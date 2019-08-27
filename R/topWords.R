75b86022f783e98ffbd152ebc1c942439bafd0a0
Jonas Rieger <jonas.rieger@tu-dortmund.de>
2019-08-27 17:07:59 +0200

    typo manpage topWords

--- R/topWords.R
+++ R/topWords.R
@@ -22,11 +22,12 @@
 #' @return Matrix of top words or, if \code{value} is \code{TRUE} a list of
 #' matrices with entries \code{word} and \code{val}.
 #' @examples
-#' texts <- list(A="Give a Man a Fish, and You Feed Him for a Day.
-#' Teach a Man To Fish, and You Feed Him for a Lifetime",
-#' B <- "So Long, and Thanks for All the Fish",
-#' C <- "A very able manipulative mathematician, Fisher enjoys a real mastery
-#' in evaluating complicated multiple integrals.")
+#' texts <- list(
+#'  A = "Give a Man a Fish, and You Feed Him for a Day.
+#'       Teach a Man To Fish, and You Feed Him for a Lifetime",
+#'  B = "So Long, and Thanks for All the Fish",
+#'  C = "A very able manipulative mathematician, Fisher enjoys a real mastery
+#'       in evaluating complicated multiple integrals.")
 #'
 #' corpus <- textmeta(meta = data.frame(id = c("A", "B", "C", "D"),
 #'   title = c("Fishing", "Don't panic!", "Sir Ronald", "Berlin"),
