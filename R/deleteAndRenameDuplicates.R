515cecf14dfed605d52f0296c230b530118c17b8
JonasRieger <jonas.rieger@tu-dortmund.de>
2020-09-09 13:26:59 +0200

    bugfix deleteAndRenameDuplicates

--- R/deleteAndRenameDuplicates.R
+++ R/deleteAndRenameDuplicates.R
@@ -4,8 +4,9 @@
 #' with the same ID but different text-component (_IDFakeDup, _IDRealDup).
 #'
 #' @param object A \code{textmeta} object as a result of a read-function.
-#' @param paragraph Logical: Should be set to \code{TRUE} if the article is a
-#' list of character strings, representing the paragraphs.
+#' @param renameRemaining Logical: Should all articles for which a counterpart with the same
+#' id exists, but which do not have the same text and - in addition - which matches (an)other
+#' article(s) in the text field be named a "fake duplicate" or not.
 #' @details Summary: Different types of duplicates:
 #'  "complete duplicates" = same ID, same information in text, same information in meta
 #'  "real duplicates" = same ID, same information in text, different information in meta
@@ -29,10 +30,34 @@
 #'
 #' duplicates <- deleteAndRenameDuplicates(object=corpus)
 #' duplicates$meta$id
+#' 
+#' texts <- list(A="Give a Man a Fish, and You Feed Him for a Day.
+#' Teach a Man To Fish, and You Feed Him for a Lifetime",
+#' A="Give a Man a Fish, and You Feed Him for a Day.
+#' Teach a Man To Fish, and You Feed Him for a Lifetime",
+#' A="A fake duplicate",
+#' B="So Long, and Thanks for All the Fish",
+#' B="So Long, and Thanks for All the Fish",
+#' C="A very able manipulative mathematician, Fisher enjoys a real mastery
+#' in evaluating complicated multiple integrals.",
+#' C="A very able manipulative mathematician, Fisher enjoys a real mastery
+#' in evaluating complicated multiple integrals.")
+#' 
+#' corpus <- textmeta(meta=data.frame(id=c("A", "A", "A", "B", "B", "C", "C"),
+#' title=c("Fishing", "Fishing2", "Fake duplicate", "Don't panic!", "towel day", "Sir Ronald", "Sir Ronald"),
+#' date=c("1885-01-02", "1885-01-02", "1885-01-03", "1979-03-04", "1979-03-05", "1951-05-06", "1951-05-06"),
+#' stringsAsFactors=FALSE), text=texts)
+#' 
+#' duplicates <- deleteAndRenameDuplicates(object=corpus)
+#' duplicates2 <- deleteAndRenameDuplicates(object=corpus, renameRemaining = FALSE)
+#' 
 #' @export deleteAndRenameDuplicates
 #'
 
 # 1. Artikel-IDs, deren IDs gleich, der Text aber unterschiedlich ist
+#    hier fallen auch all diejenigen Eintraege drunter, fuer die es eine identische
+#    ID gibt, die ggf aber auch als RealDup bereits deklariert wurde, weil es diese
+#    ID auch mit identischem text gibt.
 #     -> hier _IDFakeDup1 ... _IDFakeDupn anhaengen
 #    (hier keine Option fuer gleiche Meta-Daten, da Text unterschiedlich,
 #     also sind gleiche Meta-Daten nicht zu erwarten)
@@ -41,111 +66,77 @@
 # 3. Artikel mit komplett identischen ID, Text, Meta werden geloescht!!
 
 
-deleteAndRenameDuplicates <- function(object, paragraph = FALSE){
-  stopifnot(is.textmeta(object), is.logical(paragraph), length(paragraph) == 1)
-
+deleteAndRenameDuplicates <- function(object, renameRemaining = TRUE){
+  stopifnot(is.textmeta(object), is.logical(renameRemaining), length(renameRemaining) == 1,
+            (is.null(object$meta) || all(names(object$text) == object$meta$id)))
+  
   if (is.null(object$meta)){ #if do.meta == FALSE:
-    ind <- which(duplicated(names(object$text)) | duplicated(names(object$text),
-                                                             fromLast = TRUE))
+    ind <- which(duplicated(names(object$text)) | duplicated(names(object$text), fromLast = TRUE))
     if (length(ind) < 1) return(object)
-    if (paragraph == TRUE){
-      textvek <- unlist(lapply(object$text[ind], paste, collapse = " "))
-    }
-    else textvek <- unlist(object$text[ind])
-    # Delete duplicates of ID !and! text:
-    to_del <- ind[duplicated(textvek)]
-    if (length(to_del) > 0){
-      message(paste("delete \"complete duplicates\":", length(to_del)), appendLF = FALSE)
-      object$text <- object$text[-to_del]
+    
+    dat = data.table(i = seq_len(length(object$text))[ind],
+                     text = sapply(lapply(object$text, unlist), paste0, collapse = " ")[ind])
+    to_del = duplicated(dat[,-1])
+    if (sum(to_del) > 0){
+      message(paste("delete \"complete duplicates\":", sum(to_del)), appendLF = FALSE)
+      object$text <- object$text[-dat[to_del,i]]
       message("  next step")
     }
+    
     # Rename if text differs:
-    to_rename <- which(duplicated(names(object$text)) | duplicated(names(object$text),
-                                                                   fromLast = TRUE))
-    if (length(to_rename) > 0){
-      ind_loop = logical(length(names(object$text)))
-      ind_loop[to_rename] = TRUE
+    to_rename = duplicated(names(object$text)) | duplicated(names(object$text), fromLast = TRUE)
+    if (sum(to_rename) > 0){
       message(paste("rename \"fake duplicates\":", length(to_rename)), appendLF = FALSE)
-      for (i in na.omit(unique(names(object$text)[to_rename]))){
-        to_rename_loop <- (names(object$text) == i) & ind_loop
-        to_rename_loop[is.na(to_rename_loop)] <- FALSE
-        names(object$text)[to_rename_loop] <- paste0(names(object$text)[to_rename_loop],
-                                                     "_IDFakeDup", 1:sum(to_rename_loop))
-      }
+      dat = data.table(i = which(to_rename), id = names(object$text)[to_rename])
+      dat[, id := paste0(id, "_IDFakeDup", seq_len(.N)), by = id]
+      names(object$text)[dat[, i]] = dat[, id]
       message("  next step")
     }
     message("success")
     return(object)
   }
-  # Ansonsten existieren text und meta:
-  # 3. Artikel mit komplett identischen ID, Text, Meta werden geloescht:
-  ind <- which(duplicated(names(object$text)) | duplicated(names(object$text),
-                                                           fromLast = TRUE))
-  if (length(ind) < 1){
-    message("success")
-    return(object)
-  }
-  if (paragraph == TRUE){
-    textvek <- unlist(lapply(object$text[ind], paste, collapse = " "))
-  }
-  else textvek <- unlist(object$text[ind])
-  to_del <- ind[duplicated(object$meta[ind,]) & duplicated(textvek)]
-  if (length(to_del) > 0){
-    message(paste("delete \"complete duplicates\":", length(to_del)), appendLF = FALSE)
-    object$text <- object$text[-to_del]
-    object$meta <- object$meta[-to_del,]
-    #object$metamult <- object$metamult[-to_del]
-    message("  next step")
-    ind <- which(duplicated(names(object$text)) | duplicated(names(object$text),
-                                                             fromLast = TRUE))
-  }
-
-  # 2. Artikel mit identischer ID und Text (aber unterschiedlichen Meta-Daten):
-  if (length(ind) < 1){
-    message("success")
-    return(object)
-  }
-  if (paragraph == TRUE){
-    textvek <- unlist(lapply(object$text[ind], paste, collapse = " "))
-  }
-  else textvek <- unlist(object$text[ind])
-  text_same <- duplicated(textvek) | duplicated(textvek, fromLast = TRUE)
-  to_rename <- ind[text_same]
-  if (length(to_rename) > 0){
-    ind_loop = logical(length(names(object$text)))
-    ind_loop[to_rename] = TRUE
-    message(paste("rename \"real duplicates\":", length(to_rename)), appendLF = FALSE)
-    for (i in na.omit(unique(names(object$text)[to_rename]))){
-      to_rename_loop <- names(object$text) == i & ind_loop
-      to_rename_loop[is.na(to_rename_loop)] <- FALSE
-      new_ids <- paste0(names(object$text)[to_rename_loop], "_IDRealDup",
-                        1:sum(to_rename_loop))
-      names(object$text)[to_rename_loop] <- new_ids
-      object$meta$id[to_rename_loop] <- new_ids
-      #if (!is.null(object$metamult))
-      #  names(object$metamult)[to_rename_loop] <- new_ids
+  ind = duplicated(names(object$text)) | duplicated(names(object$text), fromLast = TRUE)
+  if(any(ind)){
+    # Ansonsten existieren text und meta:
+    # 3. Artikel mit komplett identischen ID, Text, Meta werden geloescht:
+    dat = as.data.table(cbind(seq_len(length(object$text))[ind],
+                              sapply(lapply(object$text, unlist), paste0, collapse = " ")[ind],
+                              object$meta[ind,]))
+    to_del = duplicated(dat[,-1])
+    if (sum(to_del) > 0){
+      message(paste("delete \"complete duplicates\":", sum(to_del)), appendLF = FALSE)
+      object$text <- object$text[-unlist(dat[to_del,1])]
+      object$meta <- object$meta[-unlist(dat[to_del,1]),]
+      message("  next step")
     }
-    message("  next step")
-  }
-
-  # 1. Artikel-IDs, deren IDs gleich, der Text aber unterschiedlich ist:
-  to_rename <- ind[!text_same]
-  if (length(to_rename) < 1){
-    message("success")
-    return(object)
-  }
-  ind_loop = logical(length(names(object$text)))
-  ind_loop[to_rename] = TRUE
-  message(paste("rename remaining \"fake duplicates\":", length(to_rename)), appendLF = FALSE)
-  for (i in na.omit(unique(names(object$text)[to_rename]))){
-    to_rename_loop <- names(object$text) == i & ind_loop
-    to_rename_loop[is.na(to_rename_loop)] <- FALSE
-    new_ids <- paste0(names(object$text)[to_rename_loop], "_IDFakeDup",
-                      1:sum(to_rename_loop))
-    names(object$text)[to_rename_loop] <- new_ids
-    object$meta$id[to_rename_loop] <- new_ids
-    #if (!is.null(object$metamult))
-    #  names(object$metamult)[to_rename_loop] <- new_ids
+    ind = duplicated(names(object$text)) | duplicated(names(object$text), fromLast = TRUE)
+    dat = data.table(i = seq_len(length(object$text))[ind],
+                     text = sapply(lapply(object$text, unlist), paste0, collapse = " ")[ind],
+                     id = object$meta$id[ind])
+    # 2. Artikel mit identischer ID und Text (aber unterschiedlichen Meta-Daten):
+    to_rename = duplicated(dat[, 2:3]) | duplicated(dat[, 2:3], fromLast = TRUE)
+    if (sum(to_rename) > 0){
+      message(paste("rename \"real duplicates\":", sum(to_rename)), appendLF = FALSE)
+      dat[to_rename, id := paste0(id, "_IDRealDup", seq_len(.N)), by = c("text", "id")]
+      names(object$text)[dat[to_rename, i]] = dat[to_rename, id]
+      object$meta$id[dat[to_rename, i]] = dat[to_rename, id]
+      message("  next step")
+      dat = dat[!to_rename,]
+    }
+    # 1. Artikel-IDs, deren IDs gleich, der Text aber unterschiedlich ist:
+    to_rename = duplicated(dat[, id]) | duplicated(dat[, id], fromLast = TRUE)
+    if (sum(to_rename) > 0){
+      message(paste("rename \"fake duplicates\":", sum(to_rename)), appendLF = FALSE)
+      dat[to_rename, id := paste0(id, "_IDFakeDup", seq_len(.N)), by = id]
+      names(object$text)[dat[to_rename, i]] = dat[to_rename, id]
+      object$meta$id[dat[to_rename, i]] = dat[to_rename, id]
+      dat = dat[!to_rename,]
+    }
+    if (renameRemaining && nrow(dat) > 0){
+      message(paste("\nrename remaining \"fake duplicates\":", nrow(dat)), appendLF = FALSE)
+      names(object$text)[dat[,i]] = paste0(dat[, id], "_IDFakeDup1")
+      object$meta$id[dat[,i]] = paste0(dat[, id], "_IDFakeDup1")
+    }
   }
   message("  success")
   return(object)
--- tests/testthat/test_duplist.R
+++ tests/testthat/test_duplist.R
@@ -1,43 +1,63 @@
 context("creating duplists")
 test_that("duplist", {
-
+    
     DL <- list(
+        uniqueTexts = c("a", "b", "c_IDFakeDup1", "c_IDFakeDup2", "d_IDRealDup1", "e1"),
+        notDuplicatedTexts = c("a", "b", "c_IDFakeDup1", "c_IDFakeDup2"),
+        idFakeDups = list(c = c("c_IDFakeDup1", "c_IDFakeDup2")),
+        idRealDups = list(d = c("d_IDRealDup1", "d_IDRealDup2", "d_IDRealDup3")),
+        allTextDups = list(c("d_IDRealDup1", "d_IDRealDup2", "d_IDRealDup3"), c("e1", "e2")),
+        textMetaDups = list(c("e1", "e2"))
+    )
+    class(DL) <- "duplist"
+    DL2 <- list(
         uniqueTexts = c("a", "b", "c_IDFakeDup1", "c_IDFakeDup2", "d_IDRealDup1", "e_IDRealDup1"),
         notDuplicatedTexts = c("a", "b", "c_IDFakeDup1", "c_IDFakeDup2"),
         idFakeDups = list(c = c("c_IDFakeDup1", "c_IDFakeDup2")),
         idRealDups = list(d = c("d_IDRealDup1", "d_IDRealDup2", "d_IDRealDup3"),
-            e = c("e_IDRealDup1", "e_IDRealDup2")),
+                          e = c("e_IDRealDup1", "e_IDRealDup2")),
         allTextDups = list(c("d_IDRealDup1", "d_IDRealDup2", "d_IDRealDup3"), c("e_IDRealDup1", "e_IDRealDup2")),
-        textMetaDups = list(c("e_IDRealDup1", "e_IDRealDup2"))
+        textMetaDups = list(c("e1", "e2"))
     )
-    class(DL) <- "duplist"
-
-
-########
+    class(DL2) <- "duplist"
+    
+    ########
+    corpus <- textmeta(text=list(a="A",a="A", b="B", c="C1", c="C2", d="D", d="D", d="D", e1="E", e2="E"))
+    corp <- deleteAndRenameDuplicates(corpus)
+    expect_error(duplist(corp))
+    expect_equivalent(names(corp$text), c("a", "b", "c_IDFakeDup1", "c_IDFakeDup2", "d", "e1", "e2"))
+    
+    ########
     corpus <- textmeta(meta=data.frame(id=c("a", "a", "b", "c", "c", "d", "d", "d", "e1", "e2"),
-                           title=c("a","a", "b", "c", "c", "d1", "d2", "d3", "e", "e"),
-                           date="2018-01-01", stringsAsFactors=FALSE),
+                                       title=c("a","a", "b", "c", "c", "d1", "d2", "d3", "e", "e"),
+                                       date="2018-01-01", stringsAsFactors=FALSE),
                        text=list(a="A",a="A", b="B", c="C1", c="C2", d="D", d="D", d="D", e="E", e="E"))
-    corpus <- deleteAndRenameDuplicates(corpus)
-    dl <- duplist(corpus, paragraph = FALSE)
-
+    expect_error(corpus <- deleteAndRenameDuplicates(corpus))
+    corpus <- textmeta(meta=data.frame(id=c("a", "a", "b", "c", "c", "d", "d", "d", "e1", "e2"),
+                                       title=c("a","a", "b", "c", "c", "d1", "d2", "d3", "e", "e"),
+                                       date="2018-01-01", stringsAsFactors=FALSE),
+                       text=list(a="A",a="A", b="B", c="C1", c="C2", d="D", d="D", d="D", e1="E", e2="E"))
+    corp <- deleteAndRenameDuplicates(corpus)
+    dl <- duplist(corp, paragraph = FALSE)
+    
     expect_equal(dl,DL)
+    expect_equal(dl,duplist(deleteAndRenameDuplicates(corpus, renameRemaining = FALSE)))
     expect_true(is.duplist(dl))
     expect_false(is.duplist(pi))
-########
+    ########
     corpus <- textmeta(meta=data.frame(id=c("a", "a", "b", "c", "c", "d", "d", "d", "e1", "e2"),
-                           title=c("a","a", "b", "c", "c", "d1", "d2", "d3", "e", "e"),
-                           date="2018-01-01", stringsAsFactors=FALSE),
-                       text=list(a=c("A","A2"),a=c("A","A2"), b="B", c="C1", c="C2", d="D", d="D", d="D", e="E", e="E"))
-    corpus <- deleteAndRenameDuplicates(corpus, paragraph=TRUE)
+                                       title=c("a","a", "b", "c", "c", "d1", "d2", "d3", "e", "e"),
+                                       date="2018-01-01", stringsAsFactors=FALSE),
+                       text=list(a=c("A","A2"),a=c("A","A2"), b="B", c="C1", c="C2", d="D", d="D", d="D", e1="E", e2="E"))
+    corpus <- deleteAndRenameDuplicates(corpus)
     dl <- duplist(corpus, paragraph = TRUE)
     expect_equal(dl,DL)
-########
+    ########
     corpus <- textmeta(meta=data.frame(id=c("a", "b", "c"),
-                           title=c("a", "b", "c"),
-                           date="2018-01-01", stringsAsFactors=FALSE),
+                                       title=c("a", "b", "c"),
+                                       date="2018-01-01", stringsAsFactors=FALSE),
                        text=list(a="A", b="B", c="C"))
-    corpus <- deleteAndRenameDuplicates(corpus, paragraph=TRUE)
+    corpus <- deleteAndRenameDuplicates(corpus)
     dl <- duplist(corpus, paragraph = TRUE)
     DL <- list(
         uniqueTexts = c("a", "b", "c"),
@@ -49,12 +69,12 @@ test_that("duplist", {
     )
     class(DL) <- "duplist"
     expect_equal(dl,DL)
-########
+    ########
     corpus <- textmeta(meta=data.frame(id=c("a", "a", "c"),
-                           title=c("a", "b", "c"),
-                           date="2018-01-01", stringsAsFactors=FALSE),
+                                       title=c("a", "b", "c"),
+                                       date="2018-01-01", stringsAsFactors=FALSE),
                        text=list(a="A", a="B", c="C"))
-    corpus <- deleteAndRenameDuplicates(corpus, paragraph=TRUE)
+    corpus <- deleteAndRenameDuplicates(corpus)
     dl <- duplist(corpus, paragraph = TRUE)
     DL <- list(
         uniqueTexts = c("a_IDFakeDup1", "a_IDFakeDup2", "c"),
@@ -66,8 +86,29 @@ test_that("duplist", {
     )
     class(DL) <- "duplist"
     expect_equal(dl,DL)
-
-########
+    
+    ########
+    corpus = textmeta(
+        meta = data.frame(
+            id = c("d", "a", "a", "d", "d", "d"),
+            title = c("d3", "a","a", "d1", "d2", "d3"),
+            date = "2018-01-01",
+            stringsAsFactors = FALSE),
+        text = list(d="", a="A", a="", d="D", d="D", d=""))
+    corp1 = deleteAndRenameDuplicates(corpus)
+    corpus = textmeta(
+        meta = data.frame(
+            id = c("d", "a", "a", "d", "d"),
+            title = c("d3", "a","a", "d1", "d2"),
+            date = "2018-01-01",
+            stringsAsFactors = FALSE),
+        text = list(d="", a="A", a="", d="D", d="D"))
+    corp2 = deleteAndRenameDuplicates(corpus)
+    expect_identical(corp1, corp2)
+    expect_true(all(corp1$meta$id == c("d_IDFakeDup1", "a_IDFakeDup1", "a_IDFakeDup2",
+                                       "d_IDRealDup1", "d_IDRealDup2")))
+    
+    ########
     a <- pi
     class(a) <- "duplist"
     expect_false(is.duplist(a))
@@ -77,6 +118,6 @@ test_that("duplist", {
     a <- list(uniqueTexts=NULL, notDuplicatedTexts=NULL, idFakeDups=NULL, idRealDups=NULL,
               allTextDups=NULL, textMetaDups=NULL)
     expect_false(is.duplist(a))
-
+    
     print.duplist("a")
 })
